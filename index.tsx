952b5cda43eb186b5bcde9e2090d6aa8640c45ae
Károly Sárdi <skver@tuta.io>
2026-05-25 00:41:41 +0200

    i have no clue what ai is doing at this point

--- index.tsx
+++ index.tsx
@@ -7,8 +7,7 @@ import { Divider } from "@components/Divider";
 import definePlugin, { StartAt } from "@utils/types";
 import { Channel } from "@vencord/discord-types";
 import { findByPropsLazy, findStoreLazy } from "@webpack";
-import { Avatar, ChannelStore, IconUtils, React, RelationshipStore, Text, TextInput, useMemo, useState, useStateFromStores, UserStore, createRoot } from "@webpack/common";
-import { Modal, openModal } from "../../webpack/common/modals";
+import { Avatar, ChannelStore, IconUtils, Modal, openModal, React, RelationshipStore, Text, TextInput, useMemo, useState, useStateFromStores, UserStore, createRoot } from "@webpack/common";
 import type { RenderModalProps } from "@vencord/discord-types";
 
 const PrivateChannelSortStore = findStoreLazy("PrivateChannelSortStore") as { getPrivateChannelIds: () => string[]; };
